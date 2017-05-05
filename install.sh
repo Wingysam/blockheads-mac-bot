8ebf65c04ea3d21c7464cfba477872d660030612
Wingysam <samwing@outlook.com>
2017-05-05 15:52:18 -0400

    NPM install

--- install.sh
+++ install.sh
@@ -1,6 +1,8 @@
 if [ ! -z $(which git) ]
 then
 	git clone https://github.com/Wingysam/blockheads-mac-bot
+	cd blockeheads-mac-bot
+	npm install
 	echo "Run cd blockheads-mac-bot"
 else
 	echo "You don't have git."
