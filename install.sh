8a748eb5b36ad459dae559e6aef06acd546fee2a
Wingysam <wingysam@gmail.com>
2017-05-06 07:44:12 -0400

    Installer works if you don't have git now.

--- install.sh
+++ install.sh
@@ -2,8 +2,24 @@ if [ ! -z $(which git) ]
 then
 	git clone https://github.com/Wingysam/blockheads-mac-bot
 	cd blockeheads-mac-bot
-	npm install
+	if [ ! -z $(which npm) ]
+	then
+		npm install
+	else
+		echo "You don't have NPM. Install Node.JS (nodejs.org)"
+	fi
 	echo "Run cd blockheads-mac-bot"
 else
-	echo "You don't have git."
+	echo "You don't have git. I'll use curl, and unzip."
+	if [ ! -z $(which curl) ]
+	then
+		if [ ! -z $(which unzip) ]
+		then
+			curl 'https://github.com/Wingysam/blockheads-mac-bot.git' > blockheads-mac-bot.zip
+		else
+			echo "You don't have unzip."
+		fi
+	else
+		echo "You don't have curl. That's odd, for a Mac..."
+	fi
 fi
